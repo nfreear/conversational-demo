53ec1739fad98559cee0ca6b0660195516288e52
Nick Freear <nfreear@yahoo.co.uk>
2020-01-19 08:13:37 +0000

    Bug #1, formalise `accessibility-fixes` Javascript

--- src/cf-accessibility-fixes.js
+++ src/cf-accessibility-fixes.js
@@ -1,14 +1,25 @@
 /**
- * Fix the accessibility of 'conversational-form', using WAI-ARIA, HTML5 & CSS.
+ * Fix the accessibility of 'conversational-form', using WAI-ARIA, Javascript, CSS & HTML5.
+ *
+ * @TODO These fixes are incomplete! You need to arrange accessibility testing.
+ *
+ * @license MIT
+ * @author © Nick Freear <https://nick.freear.org.uk>
  *
  * @see https://w3.org/TR/wai-aria-1.1/
  * @see https://developer.mozilla.org/en-US/docs/Web/Accessibility/ARIA/ARIA_Live_Regions
  */
 
-// Use a crude timeout!
-const CF_FIX_TIMEOUT_MS = 5000;
+// Configuration - use a crude timeout!
+const TIMEOUT_MS = 5000;
+const APPLY_SELECTOR = 'script[ src *= accessibility-fixes ][ src *= apply ]';
+const setTimeout = window.setTimeout;
+const DOC = window.document;
 
-const CF_ACCESSIBILITY_FIXES = [ {
+// ------------------------------------------------------------
+// An array of fixes, with CSS selectors.
+
+export const ACCESSIBILITY_FIXES = [ {
     // Set a 'landmark' role and label on the HTML wrapper.
     "selector": ".conversational-form",
     "attributes": {
@@ -16,7 +27,7 @@ const CF_ACCESSIBILITY_FIXES = [ {
       "role": "form",
     }
   }, {
-    // Make the message history log an ARIA 'Live Region'.
+    // Make the message history log an ARIA 'Live Region' (for screen readers users)
     "selector": ".conversational-form .scrollableInner",
     "attributes": {
       "aria-label": "Conversational log.",
@@ -42,19 +53,30 @@ const CF_ACCESSIBILITY_FIXES = [ {
   },
 ];
 
-setTimeout(() => {
+export function applyFixes(timeoutMs) {
+  setTimeout(() => {
 
-  CF_ACCESSIBILITY_FIXES.forEach(fix => {
-    const htmlElement = document.querySelector(fix.selector);
+    ACCESSIBILITY_FIXES.forEach(fix => {
+      const htmlElement = DOC.querySelector(fix.selector);
 
-    for (const key in fix.attributes) {
-      htmlElement.setAttribute(key, fix.attributes[ key ]);
-    }
-    // console.debug(htmlElement);
-  });
+      if (htmlElement) {
+        for (const key in fix.attributes) {
+          htmlElement.setAttribute(key, fix.attributes[ key ]);
+        }
+      } else {
+        console.warn('Warning, timeout? Accessibility fix not applied:', fix);
+      }
+    });
 
-  console.warn('Accessibility fixes applied:', CF_ACCESSIBILITY_FIXES);
+    console.debug('Accessibility fixes applied:', ACCESSIBILITY_FIXES);
 
-}, CF_FIX_TIMEOUT_MS);
+  }, timeoutMs || TIMEOUT_MS);
+}
+
+// ------------------------------------------------------------
+
+if (DOC.querySelector(APPLY_SELECTOR)) {
+  window.addEventListener('load', () => applyFixes()); // Not: doc..DOMContentLoaded?
+}
 
 // End.
